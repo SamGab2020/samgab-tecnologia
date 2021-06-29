956428038708212d5f70d2f2f2ae1169dc5a54d5
SamGab <gabriel-samgab@overmind.ai>
2021-06-29 17:04:02 -0300

    links politicas

--- src/pages/Elements.js
+++ src/pages/Elements.js
@@ -16,7 +16,9 @@ const IndexPage = () => (
 						<h4>Contexto</h4>
 						<p>Esta página é usada para informar os visitantes sobre minhas políticas com as informações disponibilizadas ou coletadas no app se alguém decidiu usar meu serviço. Se você optar por usar meu serviço, então concorda com a coleta e uso de informações em relação a esta política. As informações coletadas são usadas para fornecer e melhorar o serviço. Eu não vou usar ou compartilhar suas informações com ninguém, exceto conforme descrito nesta Política de privacidade. O Allcode é disponibilizado gratuitamente, para fins de utilização e produtividade no dia a dia. Não existem ADs no Allcode.</p>
 						<h4>Aplicativos de terceiros</h4>
-						<p>Link para a política de privacidade da engine ZXing, utilizada pelo Allcode. ZXing Link para a política de privacidade do Google Play Services, mecanismo também utilizado pelo Allcode. Google Play Services Ajustes para simplificar o Allcode Para simplificar a utilização do Allcode, as permissões originais de acesso aos contatos e também ao armazenamento interno foram removidas, ou seja, o app utiliza somente a permissão da câmera e utilização de rede (internet) para a correta utilização.</p>
+						<p>Link para a política de privacidade da engine <a href="https://github.com/zxing/zxing/wiki/Frequently-Asked-Questions">ZXing</a>, utilizada pelo Allcode, e também o link para a política de privacidade do <a href="https://policies.google.com/privacy">Google Play Services</a>, mecanismo também utilizado pelo app.</p>
+						<h4>Ajustes para simplificar o Allcode</h4>
+						<p>Para simplificar a utilização do Allcode, as permissões originais de acesso aos contatos e também ao armazenamento interno foram removidas, ou seja, o app utiliza somente a permissão da câmera e utilização de rede (internet) para a correta utilização.</p>
 						<h4>Segurança</h4>
 						<p>O Allcode captura apenas a informação do barcode ou do QR code para você utilizar da melhor maneira.</p>
 						<h4>Links para outros sites</h4>
