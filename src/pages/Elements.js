f07a73064792dd7c0475c6d1a2242f3df47b2ede
SamGab <gabriel-samgab@overmind.ai>
2021-10-03 12:58:50 -0300

    politica privacidade generica

--- src/pages/Elements.js
+++ src/pages/Elements.js
@@ -9,18 +9,18 @@ const IndexPage = () => (
 				<div className="inner">
 
 					<header className="major">
-						<h2>Allcode - Política de privacidade</h2>
+						<h2>SamGab Tecnologia - Política de privacidade</h2>
 					</header>
 
 					<section>
 						<h4>Contexto</h4>
-						<p>Esta página é usada para informar os visitantes sobre minhas políticas com as informações disponibilizadas ou coletadas no app se alguém decidiu usar meu serviço. Se você optar por usar meu serviço, então concorda com a coleta e uso de informações em relação a esta política. As informações coletadas são usadas para fornecer e melhorar o serviço. Eu não vou usar ou compartilhar suas informações com ninguém, exceto conforme descrito nesta Política de privacidade. O Allcode é disponibilizado gratuitamente, para fins de utilização e produtividade no dia a dia. Não existem ADs no Allcode.</p>
+						<p>Esta página é usada para informar os visitantes sobre minhas políticas com as informações disponibilizadas ou coletadas no app se alguém decidiu usar meu serviço. Se você optar por usar meu serviço, então concorda com a coleta e uso de informações em relação a esta política. As informações coletadas são usadas para fornecer e melhorar o serviço. Eu não vou usar ou compartilhar suas informações com ninguém, exceto conforme descrito nesta Política de privacidade. Os apps são disponibilizados para fins de utilização e produtividade no dia a dia. Não existem ADs nos nossos apps.</p>
 						<h4>Aplicativos de terceiros</h4>
-						<p>Link para a política de privacidade da engine <a href="https://github.com/zxing/zxing/wiki/Frequently-Asked-Questions">ZXing</a>, utilizada pelo Allcode, e também o link para a política de privacidade do <a href="https://policies.google.com/privacy">Google Play Services</a>, mecanismo também utilizado pelo app.</p>
-						<h4>Ajustes para simplificar o Allcode</h4>
-						<p>Para simplificar a utilização do Allcode, as permissões originais de acesso aos contatos foram removidas, ou seja, o app utiliza somente a permissão da câmera, armazenamento e utilização de rede (internet) para o correto funcionamento.</p>
+						<p>Link para a política de privacidade da engine <a href="https://github.com/zxing/zxing/wiki/Frequently-Asked-Questions">ZXing</a>, utilizada pelo Allcode, e também o link para a política de privacidade do <a href="https://policies.google.com/privacy">Google Play Services</a>, mecanismo também utilizado.</p>
+						<h4>Ajustes para simplificar os apps</h4>
+						<p>Para simplificar a utilização do Allcode, as permissões originais de acesso aos contatos foram removidas, ou seja, o app utiliza somente a permissão da câmera, armazenamento e utilização de rede (internet) para o correto funcionamento. Nenhuma permissão complementar é utilizada para os outros apps disponíveis.</p>
 						<h4>Segurança</h4>
-						<p>O Allcode captura apenas a informação do barcode ou do QR code para você utilizar da melhor maneira.</p>
+						<p>Os apps desenvolvidos capturam apenas as informações essenciais para você os utilizar da melhor maneira. </p>
 						<h4>Links para outros sites</h4>
 						<p>Este serviço pode conter links para outros sites. Se você clicar em um link de terceiros, será direcionado para esse site. Observe que esses sites externos não são operados por mim. Portanto, aconselho fortemente você a revisar a política de privacidade desses sites. Não tenho controle e não assumo nenhuma responsabilidade pelo conteúdo, políticas de privacidade ou práticas de quaisquer sites ou serviços de terceiros.</p>
 						<h4>Mudanças nesta política de privacidade</h4>
